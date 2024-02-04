b558421743cd4f31200abf3f271cd9833e71e037
udayadhykary <udayadhykary@gmail.com>
2024-02-04 09:48:11 +0545

    Created a table and a button was added(But may not be fully functional)

--- udaya/RadioButtons/server.r
+++ udaya/RadioButtons/server.r
@@ -1,9 +1,22 @@
 library(shiny)
 
-shinyServer(
-  function(input,output){
+server <- function(input,output){
+
+  info_data <- reactiveValues(info = data.frame(Name = character(),
+                                                  Age = numeric(),
+                                                  Gender = character()))
+    
+    observeEvent(input$add, {
+      name <- input$name
+      age <- input$age
+      gender <- input$gender
+      info_data$info <- rbind(info_data$info, data.frame(Name = name, Age = age, Gender = gender))
+      
+    })
+    
+    
+    output$info <- renderTable({info_data$info})
     output$myname <- renderText( input$name)
     output$myage <- renderText( input$age)
     output$mygender <- renderText( input$gender)
   }
-)
--- udaya/RadioButtons/ui.r
+++ udaya/RadioButtons/ui.r
@@ -5,19 +5,23 @@ library(shiny)
 ####
 
 
-shinyUI(fluidPage(
+ui<- fluidPage(
   
   titlePanel("Demonstration of shiny widgets"),
   sidebarLayout(
     sidebarPanel(("Enter the personal information"), 
                 textInput("name", "Enter your name", ""),
-                textInput("age", "Enter your age", ""), 
-                radioButtons("gender", "Select your gender", list("Male", "Female"), "")),
+                numericInput("age", "Enter your age", ""), 
+                radioButtons("gender", "Select your gender", list("Male", "Female"), ""),
+                br(),
+                actionButton("add","Add Information")),
     mainPanel(("Personal Information"),
           textOutput("myname"),
           textOutput("myage"),
-          textOutput("mygender"))
+          textOutput("mygender"),
+          br(),
+          tableOutput("info"))
   )
   
 )
-)
+
