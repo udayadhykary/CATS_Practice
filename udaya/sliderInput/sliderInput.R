b61504242e7429aa0869da73532ef27ecd702651
udayadhykary <udayadhykary@gmail.com>
2024-02-22 21:35:54 +0545

    Made changes into the sliderInput

--- udaya/sliderInput/sliderInput.R
+++ udaya/sliderInput/sliderInput.R
@@ -10,24 +10,24 @@ ui<- fluidPage(
   titlePanel("Practicing sliderInput widget in R Shiny"),
   sidebarLayout(
     sidebarPanel("Integer",
-      sliderInput("slide1", "Select the value from Slider", min = 0, max=50, value=20, step=2),
+      sliderInput("slide01", "Select the value from Slider", min = 0, max=50, value=20, step=2),
       "Decimal",
-      sliderInput("slide2", "Select the value from Slider", min = 0, max=5, value=2, step=0.2),
+      sliderInput("slide02", "Select the value from Slider", min = 0, max=5, value=2, step=0.2),
       "Range",
-      sliderInput("slide3", "Select the value from Slider", min = 0, max=500, value=c(0,200), step=20),
+      sliderInput("slide03", "Select the value from Slider", min = 0, max=500, value=c(0,200), step=20),
       "Animate",
-      sliderInput("slide4", "Select the value from Slider", min = 0, max=5, value=2, step=0.2, animate = TRUE)
+      sliderInput("slide04", "Select the value from Slider", min = 0, max=5, value=2, step=0.2, animate = TRUE)
       
     ),
     mainPanel(
-      textOutput("out1"),
-      plotOutput("plot1"),
-      textOutput("out2"),
-      plotOutput("plot2"),
-      textOutput("out3"),
-      plotOutput("plot3"),
-      textOutput("out4"),
-      plotOutput("plot4")
+      textOutput("out01"),
+      plotOutput("plot01"),
+      textOutput("out02"),
+      plotOutput("plot02"),
+      textOutput("out03"),
+      plotOutput("plot03"),
+      textOutput("out04"),
+      plotOutput("plot04")
       
     )
     
@@ -35,36 +35,36 @@ ui<- fluidPage(
 )
 server<- function(input, output){
   
-  output$out1 <- renderText(
-    paste("You selected the value: ", input$slide1))
-  output$out2 <- renderText(
-    paste("You selected the value: ", input$slide2))
-  output$out3 <- renderText(
-    paste("You selected the value: ", input$slide3))
-  output$out4 <- renderText(
-    paste("You selected the value: ", input$slide4))
+  output$out01 <- renderText(
+    paste("You selected the value: ", input$slide01))
+  output$out02 <- renderText(
+    paste("You selected the value: ", input$slide02))
+  output$out03 <- renderText(
+    paste("You selected the value: ", input$slide03))
+  output$out04 <- renderText(
+    paste("You selected the value: ", input$slide04))
 
      ## renderPlot used 
-  output$plot1 <- renderPlot({
-    x <- input$slide1
+  output$plot01 <- renderPlot({
+    x <- input$slide01
     y <- sin(x)
     plot(x, y, type = "p", main = "Plot 1", xlab = "X", ylab = "Y")
   })
   
-  output$plot2 <- renderPlot({
-    x <- input$slide2
+  output$plot02 <- renderPlot({
+    x <- input$slide02
     y <- cos(x)
     plot(x, y, type = "p", main = "Plot 2", xlab = "X", ylab = "Y")
   })
   
-  output$plot3 <- renderPlot({
-    x <- input$slide3[2] - input$slide3[1]
+  output$plot03 <- renderPlot({
+    x <- input$slide03[2] - input$slide03[1]
     y <- x^2
     plot(x, y, type = "p", main = "Plot 3", xlab = "X", ylab = "Y")
   })
   
-  output$plot4 <- renderPlot({
-    x <- input$slide4
+  output$plot04 <- renderPlot({
+    x <- input$slide04
     y <- sqrt(x)
     plot(x, y, type = "p", main = "Plot 4", xlab = "X", ylab = "Y")
   })
