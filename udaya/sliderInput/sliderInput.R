0b54fdb1a1c11330697a70e8e037cc8e48dc4bfb
PeiYu-Leu <andyleu1028tw@gmail.com>
2024-02-22 08:44:30 -0700

    Try to fix the conflict

--- udaya/sliderInput/sliderInput.R
+++ udaya/sliderInput/sliderInput.R
@@ -3,6 +3,7 @@ library(shiny)
 
 ##
 ## Test 1: make plots to display your control
+## Test 2: Show your values within only one plot. Make the plot meaningful.
 ##
 
 
@@ -10,24 +11,24 @@ ui<- fluidPage(
   titlePanel("Practicing sliderInput widget in R Shiny"),
   sidebarLayout(
     sidebarPanel("Integer",
-      sliderInput("slide1", "Select the value from Slider", min = 0, max=50, value=20, step=2),
+      sliderInput("slide11", "Select the value from Slider", min = 0, max=50, value=20, step=2),
       "Decimal",
-      sliderInput("slide2", "Select the value from Slider", min = 0, max=5, value=2, step=0.2),
+      sliderInput("slide21", "Select the value from Slider", min = 0, max=5, value=2, step=0.2),
       "Range",
-      sliderInput("slide3", "Select the value from Slider", min = 0, max=500, value=c(0,200), step=20),
+      sliderInput("slide31", "Select the value from Slider", min = 0, max=500, value=c(0,200), step=20),
       "Animate",
-      sliderInput("slide4", "Select the value from Slider", min = 0, max=5, value=2, step=0.2, animate = TRUE)
+      sliderInput("slide41", "Select the value from Slider", min = 0, max=5, value=2, step=0.2, animate = TRUE)
       
     ),
     mainPanel(
-      textOutput("out1"),
-      plotOutput("plot1"),
-      textOutput("out2"),
-      plotOutput("plot2"),
-      textOutput("out3"),
-      plotOutput("plot3"),
-      textOutput("out4"),
-      plotOutput("plot4")
+      textOutput("out11"),
+      plotOutput("plot11"),
+      textOutput("out21"),
+      plotOutput("plot21"),
+      textOutput("out31"),
+      plotOutput("plot31"),
+      textOutput("out41"),
+      plotOutput("plot41")
       
     )
     
