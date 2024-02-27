67b59bd77788add435cb635b7ca46d1c114288df
udayadhykary <udayadhykary@gmail.com>
2024-02-27 20:00:18 +0545

    Highlighting the polygons

---
+++ udaya/leaflet/choropleths/highlight/highlight.R
@@ -0,0 +1,27 @@
+library(leaflet)
+library(magrittr) 
+
+bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf)
+pal <- colorBin("YlOrRd", domain = states$density, bins = bins)
+
+states <- geojsonio::geojson_read("https://rstudio.github.io/leaflet/json/us-states.geojson", what = "sp")
+
+m<-leaflet(states) %>%
+  setView(-96, 37.8, 4) %>%
+  addProviderTiles("MapBox", options = providerTileOptions(
+    id = "mapbox.light",
+    accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN')))
+
+m %>% addPolygons(
+  fillColor = ~pal(density),
+  weight = 5,
+  opacity = 1,
+  color = "white",
+  dashArray = "3",
+  fillOpacity = 0.7,
+  highlightOptions = highlightOptions(
+    weight = 5,
+    color = "#666",
+    dashArray = "",
+    fillOpacity = 0.7,
+    bringToFront = TRUE))
