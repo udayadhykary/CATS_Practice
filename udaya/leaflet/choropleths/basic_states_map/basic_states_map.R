64e9b98798bb5f885b52cc0d140e2952df743df2
udayadhykary <udayadhykary@gmail.com>
2024-02-27 15:39:44 +0545

    Creating basic states map using leaflet package

---
+++ udaya/leaflet/choropleths/basic_states_map/basic_states_map.R
@@ -0,0 +1,11 @@
+library(leaflet)
+library(magrittr) 
+
+
+m<-leaflet(states) %>%
+  setView(-96, 37.8, 4) %>%
+  addProviderTiles("MapBox", options = providerTileOptions(
+    id = "mapbox.light",
+    accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN')))
+
+m %>% addPolygons()
