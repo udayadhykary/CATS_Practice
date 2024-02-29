023bb441376a51536f71c1b54f510bc690c41a7e
udayadhykary <udayadhykary@gmail.com>
2024-02-29 07:20:38 +0545

    Customizing the leaflet measure plugin

---
+++ udaya/leaflet/Add_ons/customization/customization.R
@@ -0,0 +1,9 @@
+m %>%
+
+  fitBounds(85.3206, 27.70169, 85.3756, 27.7306) %>%
+  addMeasure(
+    position = "bottomleft",
+    primaryLengthUnit = "meters",
+    primaryAreaUnit = "sqmeters",
+    activeColor = "#3D535D",
+    completedColor = "#7D4479")
