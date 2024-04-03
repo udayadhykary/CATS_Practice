184b1a6a76838c813dc4445c88432bb4483e3840
udayadhykary <udayadhykary@gmail.com>
2024-04-03 17:40:09 +0545

    Practiced auto arima

---
+++ udaya/time_series_analysis/improved.R
@@ -0,0 +1,52 @@
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(forecast)
+library(datasets)
+library(tseries)
+
+data(AirPassengers)
+
+class(AirPassengers)
+
+start(AirPassengers)
+
+end(AirPassengers)
+
+frequency(AirPassengers)    
+
+summary(AirPassengers)
+
+plot(AirPassengers)
+
+abline(reg=lm(AirPassengers~time(AirPassengers)))
+
+cycle(AirPassengers)
+
+plot(aggregate(AirPassengers,FUN=mean))
+
+boxplot(AirPassengers~cycle(AirPassengers))
+
+adf.test(diff(log(AirPassengers)), alternative="stationary", k=0)
+
+acf(log(AirPassengers))
+
+acf(diff(log(AirPassengers)))
+
+pacf(diff(log(AirPassengers)))
+
+fit <- arima(log(AirPassengers), c(0, 1, 1),seasonal = list(order = c(0, 1, 1), period = 12))
+
+pred <- predict(fit, n.ahead = 10*12)
+
+ts.plot(AirPassengers,2.718^pred$pred, log = "y", lty = c(1,3))
+
+
+
+
+m<- auto.arima(log(AirPassengers), trace = TRUE)
+forecasted <- forecast(m, h = 120)
+plot(forecasted)
+print(forecasted)
+
